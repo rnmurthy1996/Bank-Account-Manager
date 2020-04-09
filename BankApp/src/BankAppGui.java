c3a332c872d97cf31202a03b14479f87d70967a4
Rohan Murthy <62951244+rnmurthy1996@users.noreply.github.com>
2020-04-09 00:42:11 -0500

    Cleaned up variable names and began create user GUI

--- BankApp/src/BankAppGui.java
+++ BankApp/src/BankAppGui.java
@@ -16,17 +16,20 @@ public class BankAppGui {
 	// instance variables
 
 	private JFrame frame; // used to create frame
-	private JPanel p ;
+	
+	private JPanel accountInfo;
 	private JPanel buttons;
-	private JLabel label1; // used to label for Account number
-	private JLabel label2; // used to label account balance
-	private JLabel label3; // used to label for Account name;
-	private JLabel label4; // used to label account expire date
-	private JButton button1; // used for Deposit (action method is needed when user clicks the button)
-	private JButton button2; // used for Withdraw (action method is needed when user clicks the button)
-	private JButton button3; // used for money transfer(action method is needed when user clicks the button)
-	private JButton button4; // used to exit gui (action method is needed when user clicks the button)
-	private JComboBox<String> box; // used to select account number
+	
+	private JLabel accountNumber;
+	private JLabel accountName;
+	private JLabel expDate;
+	private JLabel balance;
+	
+	private JButton deposit;
+	private JButton withdraw;
+	private JButton transfer;
+	private JButton exit;
+	
 	private Account a;
 	
 	/**
@@ -47,34 +50,21 @@ public class BankAppGui {
 	}
 	
 	private void layoutManager() {
+		
 		frame = new JFrame("Bank Application");
 		frame.setSize(700, 400);
 		frame.getContentPane().setBackground(Color.white);
 		frame.setLayout(new FlowLayout());
 		
-		p = new JPanel();
-		p.setLayout(new FlowLayout());
-		p.setSize(50, 300);
-		frame.add(p);
+		accountInfo = new JPanel();
+		accountInfo.setLayout(new FlowLayout());
+		accountInfo.setSize(50, 300);
+		frame.add(accountInfo);
 		
 		buttons = new JPanel();
 		buttons.setLayout(new FlowLayout());
-		p.setSize(50, 300);
+		buttons.setSize(50, 300);
 		frame.add(buttons);
-	/**
-	 * 
-	 * 
-	 * Add your code to augment the design layout
-	 * please use GROUP Layout or Grid layout which ever makes appearance good.
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
 	}
 
 	/*
@@ -84,67 +74,40 @@ public class BankAppGui {
 
 		layoutManager() ;
 		
-		button1 = new JButton("Deposit");
-		button2 = new JButton("Withdraw");
-		button3 = new JButton("Transfer");
-		button4 = new JButton("Exit");
-		
-		
-		label1 = new JLabel("Account Number: " + Integer.toString(a.getAccountNumber()) + "       ");
-		label2 = new JLabel("Account Name: " + a.getName() + "       ");
-		label3 = new JLabel("Expire Date: " + (a.getExpiryDate()).substring(0,2) 
+		accountNumber = new JLabel("Account Number: " + Integer.toString(a.getAccountNumber()) + "       ");
+		accountName = new JLabel("Account Name: " + a.getName() + "       ");
+		expDate = new JLabel("Expire Date: " + (a.getExpiryDate()).substring(0,2) 
 							+ "/" + (a.getExpiryDate()).substring(2,4) +
 							"/" + (a.getExpiryDate()).substring(4) + "       ");
-		String balance = String.format("%.2f", a.getBalance());
-		label4 = new JLabel("Account Balance: " + balance + "       ");
+		String bal = String.format("%.2f", a.getBalance());
+		balance = new JLabel("Account Balance: " + bal + "       ");
 		
-		p.add(label1);
-		p.add(label2);
-		p.add(label3);
-		p.add(label4);
+		accountInfo.add(accountNumber);
+		accountInfo.add(accountName);
+		accountInfo.add(expDate);
+		accountInfo.add(balance);
 		
-		buttons.add(button1);
-		buttons.add(button2);
-		buttons.add(button3);
-		buttons.add(button4);
+		deposit = new JButton("Deposit");
+		withdraw = new JButton("Withdraw");
+		transfer = new JButton("Transfer");
+		exit = new JButton("Exit");
+		
+		buttons.add(deposit);
+		buttons.add(withdraw);
+		buttons.add(transfer);
+		buttons.add(exit);
 
 		frame.setVisible(true);
-		button1.addActionListener(new DepositMoney());
-		button2.addActionListener(new WithdrawMoney());
-		button3.addActionListener(new TransferMoney());
-		button4.addActionListener(new Exit());
 		
-		/*
-		 * Add your code here to create further interface.
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 */
-		
+		deposit.addActionListener(new DepositMoney());
+		withdraw.addActionListener(new WithdrawMoney());
+		transfer.addActionListener(new TransferMoney());
+		exit.addActionListener(new Exit());
 	}
 
 //Internal class to perform action associated to the button.
 	private class DepositMoney implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			// implement the Code to handle button click goes here
-			
-			
-			
-			
-			
-			
 			
 			
 			
@@ -154,17 +117,6 @@ public class BankAppGui {
 	//Internal class to perform action associated to the button.
 	private class WithdrawMoney implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			// implement the Code to handle button click goes here
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
 			
 			
@@ -174,16 +126,6 @@ public class BankAppGui {
 	//Internal class to perform action associated to the button.
 	private class TransferMoney implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
-			// implement the Code to handle button click goes here
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
 			
 			
@@ -195,40 +137,6 @@ public class BankAppGui {
 		public void actionPerformed(ActionEvent event) {
 			// implement the Code to handle button click goes here
 			frame.dispose();
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 		}
 	}
-
-	/**
-	 * Add your code to perform any extra action items.
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
-	
-	
 }
